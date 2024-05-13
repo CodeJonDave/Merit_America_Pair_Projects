6e12cd682f87947d33cecf10950063b5d956fa56
Jonathan Sutton <jonathan.d.sutton2024@gmail.com>
2024-05-12 19:00:07 -0700

    Update MakeChange.java

--- module-1-pair-programming-1/src/main/java/com/techelevator/MakeChange.java
+++ module-1-pair-programming-1/src/main/java/com/techelevator/MakeChange.java
@@ -15,12 +15,22 @@ public class MakeChange {
     public static void main(String[] args) {
         while (true) {
             Scanner input = new Scanner(System.in);
+         
             System.out.println("Please enter the amount of the bill: ");
             double bill = Double.parseDouble(input.nextLine());
+         
             System.out.println("Please enter the amount tendered:");
             double amount = Double.parseDouble(input.nextLine());
+            
             System.out.println("The change required is " + (amount - bill));
 
+            System.out.println("Do you want to make another calculation? (y/n)");
+            response = userInput.nextLine();
+
+            if (!response.equalsIgnoreCase("y")) {
+                System.out.println("Thank you for using this service!");
+                break;
+            }
 
         }
 
